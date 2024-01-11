2d27b5565b339d582d6bdab8d660019eb13c6405
msa-sadeghi <sadeghi.msai@gmail.com>
2024-01-11 21:03:39 +0330

    a
    a

---
+++ test_platformer/temp.py
@@ -0,0 +1,24 @@
+class Player:
+    def __init__(self,x,y, right_images, left_images) -> None:
+        self.right_images = right_images
+        self.left_images = left_images
+
+
+    def move(self, keys):
+        
+
+
+
+player_1_right_images = []
+player_1_left_images = []
+for 
+
+
+player_2_right_images = []
+player_2_left_images = []
+for 
+
+
+
+p1 = Player(100, player_1_left_images, player_1_right_images)
+p1 = Player(100, player_2_left_images, player_2_right_images)
