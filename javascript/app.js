1503493fc218806634be39a6ebfde514c3384b4b
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-10-27 21:41:56 +0330

    calculate

---
+++ javascript/app.js
@@ -0,0 +1,54 @@
+// let person = {
+//     name : "reza",
+//     age : 30
+// }
+
+// console.log(person.name)
+// console.log(person.age)
+// console.log(person["name"])
+// console.log(person["age"])
+
+// let colors = ["red", "green", "blue"]
+// console.log(colors[0])
+// console.log(colors[1])
+// console.log(colors[2])
+
+// let num = 123
+// let num2 = String(num)
+// let num2 = num.toString()
+// let  num3 = num + ""
+
+// let x  = "12"
+// let x1 = Number(x)
+// let x4 = parseInt("123")
+// let x42 = parseFloat("123.4")
+
+// console.log(Boolean(1))
+// console.log(Boolean(0))
+// console.log(Boolean(-1))
+
+// console.log("5" + 3)
+// console.log("5" - 3)
+// console.log("5" / 3)
+// console.log("5" % 3)
+
+
+
+
+// numbers = -1
+// if(Boolean(numbers)){
+//     console.log("not empty")
+// }else{
+//     console.log("empty")
+// }
+
+let x = "12"
+let y = 12
+
+if(x === y){
+    console.log("equal")
+}
+console.log(Number.isInteger(y))
+
+let c = [1,2,3]
+console.log(Array.isArray(c))
