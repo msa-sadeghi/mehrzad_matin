7e7d5f997ca8484ad07e8d7e163be9d2f9feacd3
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-11-03 21:24:19 +0330

    d

--- javascript/app.js
+++ javascript/app.js
@@ -21,8 +21,6 @@ function convertTemperature(){
         }
 
     }
-
-
     tempResult.textContent = `نتیجه ${result}`
     tempResult.style.display = "block"
 
---
+++ javascript/app2.js
@@ -0,0 +1,81 @@
+let x1 = 5
+// let y1 = ++x1
+// console.log(x1)
+// console.log(y1)
+// console.log("\n")
+// let x2 = 5
+// let y2 = x2++
+// console.log(x2)
+// console.log(y2)
+
+// y2 += 1
+// console.log(y2)
+
+// y2 = y2 + 1
+// console.log(y2)
+// console.log("\n")
+
+// let score = 0
+// --score
+// console.log(score)
+
+
+// console.log(5 == 5)
+// console.log(5 === "5")
+
+// let age = 18
+
+// if(age >= 18 && age <= 25){
+//     console.log("adult")
+// }else if(age >= 13 && age <=17){
+//     console.log("teenager")
+// }
+
+// // || &&
+
+// // not   !
+
+// let isLoggedIn = false
+// if(!isLoggedIn){
+//     console.log("not valid")
+// }
+
+// let  age2 = 12
+
+// let status = age2 <= 17 ? "not valid user" : "valid user"
+// console.log(status)
+
+// let score = 85
+// let grade = score >= 90 ? 
+// "great" : score >= 75 ? "good" : 
+// score >= 50 ? "not good" :  "blalal"
+
+
+// console.log(grade)
+
+let temp = 35
+if(temp > 30){
+    console.log("1")
+}else if(temp == 17){
+    console.log("2")
+}else{
+    console.log("5")
+}
+
+let month = "jan"
+let season
+
+switch(month){
+    case "jan":
+    case "feb":
+        season = "1"
+        break
+    case "apr":
+    case "j":
+        season = "2"
+        break
+
+    default:
+        month = "invalid"
+}
+console.log(dayname)
