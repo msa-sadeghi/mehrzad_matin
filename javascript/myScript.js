bb50c47eab3be84dd4c5c605dea188bea41517e8
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-11-26 21:35:44 +0330

    a

--- javascript/myScript.js
+++ javascript/myScript.js
@@ -2,6 +2,9 @@ let basicNum1 = document.getElementById("basicNum1")
 let basicNum2 = document.getElementById("basicNum2")
 let basicOp = document.getElementById("basicOp")
 let basicResult = document.getElementById("basicResult")
+let historyListElement = document.getElementById("historyList")
+
+historyList = []
 
 function add(a, b){
     return a + b
@@ -25,12 +28,48 @@ function rem(a, b){
 
 function calculateBasic(){
     let basicOpVal = basicOp.value
+    let basicOpValText 
     let basicNum1Val = Number(basicNum1.value)
     let basicNum2Val = Number(basicNum2.value)
     let res
     if(basicOpVal === 'add'){
         res = add(basicNum1Val, basicNum2Val)
+        basicOpValText = "+"
+    }else if(basicOpVal === 'subtract'){
+        
+        res = sub(basicNum1Val, basicNum2Val)
+        basicOpValText = "-"
     }
     basicResult.innerHTML = res
     basicResult.style.display = "block"
+    const basicOpText = `${res} = ${basicNum1Val} ${basicOpValText} ${basicNum2Val}`
+    addToHistory(basicOpText)
+}
+
+
+
+function addToHistory(calculation){
+    historyList.unshift(calculation)
+    if(historyList.length > 10){
+        historyList.pop()
+    }
+    updateHistoryList()
+}
+
+function updateHistoryList(){
+    if(historyList.length === 0){
+
+        historyListElement.innerHTML = '<p/>تاریخچه ای وجود ندارد<p>'
+        return
+    }
+    historyListElement.innerHTML = historyList.map(item => 
+        `<div class ="history-item">${item}</div>`
+    ).join('')
+}
+
+
+
+function clearHistory(){
+    historyList = []
+     historyListElement.innerHTML = '<p/>تاریخچه ای وجود ندارد<p>'
 }
