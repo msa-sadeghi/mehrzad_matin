2d07d6c871be773c021ddb1f977ff2b0aabb7fa8
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-12-15 21:59:22 +0330

    ccc

--- javascript/app.js
+++ javascript/app.js
@@ -1,15 +1,46 @@
-const btn = document.querySelector("#theme");
-const addBtn = document.querySelector("#add");
-const body = document.body;
-btn.addEventListener("click", () => {
-    body.classList.toggle("dark");
-    body.classList.toggle("light");
-    
-});
-addBtn.addEventListener("click", () => {
-    const card = document.createElement("div");
-    card.classList.add("card"); 
-    card.innerHTML = `<h2>another card</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere eius veritatis dicta at necessitatibus omnis voluptas sint. Molestias ad magni architecto. Nihil nisi rem ad dolorum eveniet saepe vero aut?</p>`;
-    body.appendChild(card);
-});
+const todoInput = document.getElementById("todoInput")
+const todoList = document.getElementById("todoList")
+
+function IDGenerator(){
+    let id = 0
+    return function(){
+        id++
+        return id
+    }
+}
+
+let idG = IDGenerator()
+
+function Item(name){
+    this.name = name
+    this.id = idG()
+}
+
+let  allTodoItems = []
+
+function addTodo(){
+    let inputValue = todoInput.value.trim()
+    let newItem =new Item(inputValue)
+    allTodoItems.push(newItem)
+    localStorage.setItem('todos', JSON.stringify(allTodoItems))  
+    renderList()
+}
+
+function renderList(){
+    todoList.innerHTML = ''
+    allTodoItems.forEach((item)=>{
+    let newDiv = document.createElement('div')
+    newDiv.classList.add('todo-item')
+    let span = document.createElement("span")
+    span.textContent = item.name
+    let i = document.createElement("i")
+    i.className = "fa-solid fa-trash-can"
+    i.setAttribute("id", item.id)
+    i.addEventListener('click', (e)=>{
+       
+    })
+    newDiv.append(span, i)
+    todoList.append(newDiv)
+    })
+}
+
