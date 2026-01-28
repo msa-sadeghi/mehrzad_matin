e7862ab3718dfdf90b41d643778fbee412f8f2b6
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-01-28 21:58:07 +0330

    codes

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,27 @@
-const btn = document.querySelector("button")
-const log = document.getElementById("log")
-btn.addEventListener("click", (e)=>{
-    e.preventDefault()
-    log.innerHTML += "hello"
+fetch("https://jsonplaceholder.typicode.com/users")
+.then(response => response.json())
+.then(data => {
+    setTimeout(()=>{
+    displayUsers(data)
+        },2000)
 })
+.catch(error=>console.log(error))
+
+let container = document.getElementById("container")
+let loadingElement = document.getElementById("loading")
+function displayLoading(flag){
+  
+    loadingElement.style.display = flag ? 'block' : 'none'
+}
+
+function displayUsers(users){
+    displayLoading(true)
+        users.forEach(element => {
+        const userDiv = document.createElement("div")
+        userDiv.innerText = element.name
+
+        container.append(userDiv)
+    });
+    
+    displayLoading(false)
+}
