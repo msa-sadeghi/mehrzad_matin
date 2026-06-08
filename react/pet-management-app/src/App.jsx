299739a44c1dbdac2b8085d2952145527a181548
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-06-08 21:59:01 +0330

    git aa

--- react/pet-management-app/src/App.jsx
+++ react/pet-management-app/src/App.jsx
@@ -1,55 +1,58 @@
-import { useState } from "react"
-import TodoList from "./components/TodoList"
-import TodoForm from "./components/TodoForm"
-import FilterButtons from "./components/FilterButtons"
-import './assets/css/style.css'
-function App(){
-    const [todos, setTodos] = useState(()=>{
-        const saved = localStorage.getItem("todos")
-        return saved ? JSON.parse(saved) : []
-    })
-    const [filter, setFilter] = useState('All')
+import { useEffect, useState } from "react";
+import "./assets/css/style.css";
+import FilterButtons from "./components/FilterButtons";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+function App() {
+  const [todos, setTodos] = useState(() => {
+    const saved = localStorage.getItem("todos");
+    return saved ? JSON.parse(saved) : [];
+  });
+  const [filter, setFilter] = useState("All");
 
-    const filteredTodos = todos.filter(t => {
-        if(filter === 'Active') return !t.completed
-        if(filter === 'Completed') return t.completed
-        return true
-    })
+  const filteredTodos = todos.filter((t) => {
+    if (filter === "Active") return !t.completed;
+    if (filter === "Completed") return t.completed;
+    return true;
+  });
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((t) => t.id !== id));
+  };
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((t) => (t.id === id ? { ...t, completed: !t.completed } : t)),
+    );
+  };
+  const addTodo = (text) => {
+    const newTodo = {
+      id: Date.now(),
+      text: text,
+    };
+    setTodos([...todos, newTodo]);
+  };
 
-    const deleteTodo = (id)=>{
-        const newTodos = todos.filter(t=>t.id !== id)
-        setTodos(newTodos)
-        localStorage.setItem('todos', JSON.stringify(newTodos))
-    }
-    const toggleTodo = (id)=>{
-        const newTodos = todos.map(
-        t=>t.id === id ? {...t, completed: !t.completed} : t)
-        setTodos(newTodos)    
-        localStorage.setItem('todos', JSON.stringify(newTodos))
-    }
-    const addTodo = (text)=>{
-        const  newTodo = {
-            id:Date.now(),
-            text:text
-        }
-        const newTodos = [...todos, newTodo]
-        setTodos(newTodos)
-        localStorage.setItem("todos",  JSON.stringify(newTodos))
-    }
-    return(
-        <div style={{ width:"400px", margin:"50px auto"}}>
-            <h1>Todo App</h1>
-            <TodoForm 
-            addTodo={addTodo}
-            />
-            <FilterButtons setFilter={setFilter}/>
-            <TodoList
-            todos={filteredTodos}
-            toggleTodo={toggleTodo}
-            deleteTodo={deleteTodo}
-            />
-        </div>
-    )
+  const saveTodo = (id, newText) => {
+    setTodos((prev) =>
+      prev.map((t) => (t.id === id ? { ...t, text: newText } : t)),
+    );
+  };
+
+  return (
+    <div style={{ width: "400px", margin: "50px auto" }}>
+      <h1>Todo App</h1>
+      <TodoForm addTodo={addTodo} />
+      <FilterButtons setFilter={setFilter} />
+      <TodoList
+        todos={filteredTodos}
+        toggleTodo={toggleTodo}
+        deleteTodo={deleteTodo}
+        saveTodo={saveTodo}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- react/pet-management-app/src/components/TodoList.jsx
+++ react/pet-management-app/src/components/TodoList.jsx
@@ -1,20 +1,19 @@
-import TodoItem from "./TodoItem"
+import TodoItem from "./TodoItem";
 
-function TodoList({todos, deleteTodo, toggleTodo}){
-    return(
-        <div>
-            {
-                todos.map((t) => (
-                    <TodoItem
-                        key={t.id}
-                        todo={t}
-                        deleteTodo={deleteTodo}
-                        toggleTodo={toggleTodo}
-                    />
-                ))
-            }
-        </div>
-    )
+function TodoList({ todos, deleteTodo, toggleTodo, saveTodo }) {
+  return (
+    <div>
+      {todos.map((t) => (
+        <TodoItem
+          key={t.id}
+          todo={t}
+          deleteTodo={deleteTodo}
+          toggleTodo={toggleTodo}
+          saveTodo={saveTodo}
+        />
+      ))}
+    </div>
+  );
 }
 
-export default TodoList
+export default TodoList;
