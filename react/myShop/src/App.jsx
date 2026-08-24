9a4d93ce701c432ea202afc6ede0ef5817533fa9
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-08-24 21:39:14 +0330

    s

--- react/myShop/src/App.jsx
+++ react/myShop/src/App.jsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
 import "./assets/css/style.css";
-import ProductList from "./features/products/components/ProductList";
+import ProductList, {
+  SearchForm,
+} from "./features/products/components/ProductList";
 function App() {
-  const fruits = [
+  const [fruits, setFruits] = useState([
     { id: 1, name: "apple", price: 234 },
     { id: 2, name: "bana", price: 456 },
     { id: 3, name: "orange", price: 4556 },
-  ];
+  ]);
+  const handleRemove = (id) => {
+    console.log("clicked");
+    const newFruits = fruits.filter((p) => p.id !== id);
+    setFruits(newFruits);
+  };
   return (
     <>
-      <ProductList />
+      <SearchForm />
+      <ProductList products={fruits} handleRemove={handleRemove} />
     </>
   );
 }
--- react/myShop/src/features/products/components/ProductList.jsx
+++ react/myShop/src/features/products/components/ProductList.jsx
@@ -1,11 +1,28 @@
-export default function ProductList() {
+export default function ProductList({ products, handleRemove }) {
   return (
     <div>
       <ul>
-        {ProductList.map((p) => (
-          <li>{p.name}</li>
+        {products.map((p) => (
+          <li key={p.id}>
+            {p.name}
+            <button onClick={() => handleRemove(p.id)}>delete</button>
+          </li>
         ))}
       </ul>
     </div>
+  );
+}
+
+export function SearchForm() {
+  function handleSubmit(event) {
+    event.preventDefault(); // جلوگیری از رفرش شدن صفحه
+    console.log("فرم ارسال شد");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" />
+      <button type="submit">جستجو</button>
+    </form>
   );
 }
