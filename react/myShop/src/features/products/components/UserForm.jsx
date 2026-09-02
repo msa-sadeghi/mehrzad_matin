9e03116a6c44321e5fec06985e77975287b17cbd
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-09-02 21:30:25 +0330

    S

--- react/myShop/src/features/products/components/UserForm.jsx
+++ react/myShop/src/features/products/components/UserForm.jsx
@@ -1,14 +1,27 @@
 import { useState } from "react";
 
 function UserForm() {
-  const [user, setUser] = useState({ name: "", age: 0 });
-
-  function handleNameChange(event) {
-    user.name = event.target.value; 
-    setUser(user);
-  }
-
-  return <input value={user.name} onChange={handleNameChange} />;
+  const [todos, setTodos] = useState([
+    { id: 1, title: "task # 1", completed: false },
+    { id: 2, title: "task # 2", completed: false },
+    { id: 3, title: "task # 3", completed: false },
+    { id: 4, title: "task # 4", completed: false },
+    { id: 5, title: "task # 5", completed: false },
+  ]);
+  return (
+    <>
+      <input type="text" />
+      <button>Add</button>
+      {todos.map((t) => (
+        <div>
+          <span key={t.id}>{t.title}</span>
+          <button>remove</button>
+          <button>update</button>
+          <button>toggle</button>
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default UserForm;
